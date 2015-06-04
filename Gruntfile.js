15d1261c27b93cba22a55590dd7c5d2327b2f5af
conanmy <mayue@280F20DC35B844E.internal.baidu.com>
2015-06-05 10:26:51 +1200

    ninit

---
+++ Gruntfile.js
@@ -0,0 +1,178 @@
+/*eslint-env node*/
+/*global module:false*/
+module.exports = function(grunt) {
+
+    grunt.initConfig({
+
+        dir: {
+            webapp: 'src',
+            tests: 'test',
+            dist: 'dist',
+            bower_components: 'bower_components',
+            localServerTestUrl: 'http://localhost:8080/test-resources'
+        },
+
+        tests: {
+            opaTimeout: 900000
+        },
+
+        connect: {
+            options: {
+                port: 8080,
+                hostname: '*'
+            },
+            src: {
+                options: {
+                    open: {
+                        target: 'http://localhost:8080/index.html'
+                    }
+                }
+            },
+            dist: {
+                options: {
+                    open: {
+                        target: 'http://localhost:8080/build.html'
+                    }
+                }
+            }
+        },
+
+        openui5_connect: {
+            options: {
+                resources: [
+                    '<%= dir.bower_components %>/openui5-sap.ui.core/resources',
+                    '<%= dir.bower_components %>/openui5-sap.m/resources',
+                    '<%= dir.bower_components %>/openui5-sap.ui.layout/resources',
+                     '<%= dir.bower_components %>/openui5-sap.ui.commons/resources',
+                    '<%= dir.bower_components %>/openui5-themelib_sap_bluecrystal/resources'
+                ]
+            },
+            src: {
+                options: {
+                    appresources: ['.'],
+                    testresources: ['<%= dir.tests %>']
+                }
+            },
+            dist: {
+                options: {
+                    appresources: '.',
+                    testresources: ['<%= dir.tests %>']
+                }
+            }
+        },
+
+        openui5_preload: {
+            component: {
+                options: {
+                    resources: {
+                        cwd: '<%= dir.webapp %>',
+                        prefix: 'sap/ui/demo/masterdetail'
+                    },
+                    dest: '<%= dir.dist %>'
+                },
+                components: true,
+                compress: true
+            }
+        },
+
+        clean: {
+            dist: '<%= dir.dist %>/'
+        },
+
+        copy: {
+            dist: {
+                files: [{
+                    expand: true,
+                    cwd: '<%= dir.webapp %>',
+                    src: [
+                        '**',
+                        '!test/**'
+                    ],
+                    dest: '<%= dir.dist %>'
+                }]
+            }
+        },
+
+        eslint: {
+            options: {
+                quiet: true
+            },
+
+            all: ['<%= dir.tests %>', '<%= dir.webapp %>'],
+            webapp: ['<%= dir.webapp %>']
+        },
+
+        qunit: {
+            options: {
+                /* for debugging*/
+                '--remote-debugger-autorun': 'yes',
+                '--remote-debugger-port': 8000
+            },
+
+            unit: {
+                options: {
+                    urls: [
+                        '<%= dir.localServerTestUrl %>/unit/unitTests.qunit.html'
+                    ]
+                }
+
+            },
+            opa: {
+                options: {
+                    urls: [
+                        '<%= dir.localServerTestUrl %>/integration/opaTests.qunit.html'
+                    ],
+                    // same as qunits timeout 90 seconds since opa test might take a while
+                    timeout: '<%= tests.opaTimeout %>'
+                }
+            },
+            opaPhone: {
+                options: {
+                    urls: [
+                        '<%= dir.localServerTestUrl %>/integration/opaTestsPhone.qunit.html'
+                    ],
+                    // same as qunits timeout 90 seconds since opa test might take a while
+                    timeout: '<%= tests.opaTimeout %>'
+                },
+
+                page: {
+                    settings: {
+                        userAgent: "Mozilla/5.0 (iPhone; CPU iPhone OS 5_0_1 like Mac OS X) AppleWebKit/534.48 (KHTML, like Gecko) Version/5.1 Mobile/9A406 Safari/7534.48.3" // iOS userAgent string
+                    }
+                }
+            }
+        }
+
+    });
+
+    // These plugins provide necessary tasks.
+    grunt.loadNpmTasks('grunt-contrib-connect');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-openui5');
+    grunt.loadNpmTasks('grunt-eslint');
+    grunt.loadNpmTasks('grunt-contrib-qunit');
+
+    // Server task
+    grunt.registerTask('serve', function(target) {
+        grunt.task.run('openui5_connect:' + (target || 'src') + ':keepalive');
+    });
+
+    // Linting task
+    grunt.registerTask('lint', ['eslint:all']);
+
+    // Build task
+    grunt.registerTask('build', ['clean', 'openui5_preload', 'copy']);
+    grunt.registerTask('buildRun', ['build', 'serve:dist']);
+
+    // Test task
+    grunt.registerTask('test', ['openui5_connect:src', 'qunit:unit', 'qunit:opa']);
+    grunt.registerTask('unitTest', ['openui5_connect:src', 'qunit:unit']);
+    grunt.registerTask('opaTest', ['openui5_connect:src', 'qunit:opa']);
+
+    // Default task
+    grunt.registerTask('default', [
+        'lint:all',
+        'test'
+    ]);
+};
---
+++ src/Component.js
@@ -0,0 +1,86 @@
+﻿/*!
+ * ${copyright}
+ */
+
+sap.ui.define([
+    "sap/ui/core/UIComponent",
+    "sap/ui/model/resource/ResourceModel",
+    "sap/ui/model/odata/v2/ODataModel"
+], function(UIComponent, ResourceModel, ODataModel) {
+    "use strict";
+
+    return UIComponent.extend("scenario.xmlview.Component", {
+
+        metadata: {
+            "rootView": "scenario.xmlview.view.App",
+            "dependencies": {
+                "minUI5Version": "1.28.0",
+                "libs": ["sap.ui.core", "sap.m", "sap.ui.layout"]
+            },
+
+            "config": {
+                "i18nBundle": "scenario.xmlview.i18n.i18n",
+                "serviceUrl": "here/goes/your/serviceUrl/"
+            }
+        },
+
+        createContent: function() {
+            var oModel = new ODataModel(this.getMetadata().getConfig().serviceUrl, {
+                useBatch: false
+            });
+            this.setModel(oModel);
+            var oLayout =new sap.ui.layout.VerticalLayout();
+
+            // in callback handler map OData to JSon Model - Workaround till know more
+            var handler = function(oEvent) {
+                var mapCallback = function(oContext) {
+                    var oData = jQuery.extend({}, oContext.getObject());
+                    var aLookup = oData.Lookup.__list;
+                    oData.Path = oContext.getPath();
+                    oData.Lookup = aLookup.map(function(sPath) {
+                        return oModel.getContext('/' + sPath).getObject();
+                    });
+                    return oData;
+                };
+
+                oBinding.detachChange(handler);
+
+                //map fields collection to json format
+                var aFields = oEvent.oSource.getContexts().map(mapCallback);
+                var oViewModel = new sap.ui.model.json.JSONModel({
+                    Fields: aFields
+                });
+
+                // Create view passing the JSON Model
+                var oDetailView = sap.ui.view({
+                    preprocessors: {
+                        xml: {
+                            models: {
+                                vm: oViewModel
+                            }
+                        }
+                    },
+                    type: sap.ui.core.mvc.ViewType.XML,
+                    viewName: "scenario.xmlview.view.Detail"
+                });
+                oDetailView.setModel(oModel);
+                // oLayout.addItem(oDetailView);
+                oLayout.addContent(oDetailView);
+            };
+
+            // Call OData service to get the Fields entity with Lookup Value inlined
+            var oBinding = oModel.bindList("/Fields", null, null, null, {
+                expand: "Lookup"
+            }).initialize();
+            oBinding.attachRefresh(function() {
+                oBinding.getContexts();
+            });
+            oBinding.attachChange(handler);
+
+            return oLayout;
+        }
+
+
+    });
+
+});
