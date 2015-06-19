339f53ea216ffe70ed9b315fa1ff79c2108193dd
conanmy <mayue@280F20DC35B844E.internal.baidu.com>
2015-06-19 17:45:21 +1200

    update a condition

---
+++ src/controller/Conditions.controller.js
@@ -0,0 +1,9 @@
+sap.ui.define(["scenario/xmlview/controller/BaseController"], function(BaseController) {
+    "use strict";
+    return BaseController.extend("scenario.xmlview.controller.Conditions", {
+    	saveIt: function(oEvent) {
+    		var targetContext = oEvent.getSource().getBindingContext();
+    		targetContext.oModel.submitChanges();
+    	}
+	});
+});
