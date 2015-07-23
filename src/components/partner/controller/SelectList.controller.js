43cbb4bec9ff1f0e59b49a813886cf1921fc3dab
FrankMa <semstar2010@gmail.com>
2015-07-23 18:21:21 +1200

    format indent as tab for partner step

--- src/components/partner/controller/SelectList.controller.js
+++ src/components/partner/controller/SelectList.controller.js
@@ -1,39 +1,40 @@
 sap.ui.define(["aklc/cm/controller/BaseController"], function(BaseController) {
-    "use strict";
-    return BaseController.extend("aklc.cm.components.partner.controller.SelectList", {
-        onInit: function(evt) {
-            this.targetList = this.getView().byId("selectList");
-        },
+	"use strict";
+	return BaseController.extend("aklc.cm.components.partner.controller.SelectList", {
+		onInit: function(evt) {
+			this.targetList = this.getView().byId("selectList");
+		},
 
-        onSearch: function(oEvt) {
+		onSearch: function(oEvt) {
 
-            // add filter for search
-            var aFilters = [];
-            var sQuery = oEvt.getSource().getValue();
-            if (sQuery && sQuery.length > 0) {
-                var filter = new sap.ui.model.Filter(
-                    "Description",
-                    sap.ui.model.FilterOperator.Contains,
-                    sQuery
-                );
-                aFilters.push(filter);
-            }
+			// add filter for search
+			var aFilters = [];
+			var sQuery = oEvt.getSource().getValue();
+			if (sQuery && sQuery.length > 0) {
+				var filter = new sap.ui.model.Filter(
+					"Description",
+					sap.ui.model.FilterOperator.Contains,
+					sQuery
+				);
+				aFilters.push(filter);
+			}
 
-            // update list binding
-            var binding = this.targetList.getBinding("items");
-            binding.filter(aFilters, "Application");
-        },
+			// update list binding
+			var binding = this.targetList.getBinding("items");
+			binding.filter(aFilters, "Application");
+		},
 
-        onSelectionChange: function(oEvt) {
+		onSelectionChange: function(oEvt) {
 
-            var listItem = oEvt.mParameters.listItem;
-            var itemPath = listItem.getBindingContextPath();
-            this.getEventBus().publish(
-                'SelectList',
-                'selected',
-                {path: itemPath}
-            );
-            this.targetList.removeSelections();
-        }
-    });
+			var listItem = oEvt.mParameters.listItem;
+			var itemPath = listItem.getBindingContextPath();
+			this.getEventBus().publish(
+				'SelectList',
+				'selected', {
+					path: itemPath
+				}
+			);
+			this.targetList.removeSelections();
+		}
+	});
 });
