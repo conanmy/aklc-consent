44c6e3fd6aff4458de0122e0f1ccce71262f4cb9
FrankMa <semstar2010@gmail.com>
2015-08-07 15:31:50 +1200

    customize object list item

---
+++ src/components/partner/controls/PartnerObjectListItem.js
@@ -0,0 +1,41 @@
+sap.ui.define(["sap/m/ObjectListItem"],
+	function(ObjectListItem) {
+		"use strict";
+		var PartnerObjectListItem = ObjectListItem.extend("aklc.cm.components.partner.controls.PartnerObjectListItem", {
+			metadata: {
+				library : "sap.m",
+				properties: {
+					deletable: {
+						type : "boolean",
+						group : "Misc",
+						defaultValue : true
+					}
+				}
+			},
+			renderer: "sap.m.ObjectListItemRenderer"
+		});
+
+		PartnerObjectListItem.prototype.getModeControl = function() {
+			var sMode = this.getMode(),
+				mListMode = sap.m.ListMode;
+
+			if (!sMode || sMode == mListMode.None) {
+				return;
+			}
+
+			if (sMode == mListMode.Delete) {
+				if (!this.getDeletable()) {
+					return;
+				}
+				return this.getDeleteControl();
+			}
+
+			if (sMode == mListMode.MultiSelect) {
+				return this.getMultiSelectControl();
+			}
+
+			return this.getSingleSelectControl();
+		};
+
+		return PartnerObjectListItem;
+	}, /* bExport= */ true);
